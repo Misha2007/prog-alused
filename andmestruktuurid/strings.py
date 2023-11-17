d88743d22eee978100f9cd299920dd7ffee1007c
Misha2007 <mishadrogov@gmail.com>
2023-11-17 09:56:29 +0200

    andmestruktuurid - solution

---
+++ andmestruktuurid/strings.py
@@ -0,0 +1,20 @@
+first_name = "James"
+last_name = "Bond"
+
+full_name = first_name + " " + last_name
+self_description_sentence = f"My name is {last_name}, {full_name}."
+
+# print(self_description_sentence)
+
+cake = "vahukoormarjadtäidispõhi"
+
+print(f"{cake[:8]}\n{cake[8:14]}\n{cake[14:20]}\n{cake[20:]}")
+
+original_string = "Programming is fun!"
+
+
+every_other = original_string[::2]
+
+backwards = original_string[::-1]
+
+first_word_reversed = original_string[10:-20:-1]
